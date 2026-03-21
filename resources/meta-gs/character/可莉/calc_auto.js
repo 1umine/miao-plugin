71f06f17cc4cccf8cfb9dbfd205e70d03709636e
1umine <lyyme34@gmail.com>
2026-03-21 22:45:15 +0800

    更新可莉伤害计算

--- resources/meta-gs/character/可莉/calc_auto.js
+++ resources/meta-gs/character/可莉/calc_auto.js
@@ -33,6 +33,25 @@ export const details = [{
       avg: ta.avg * 8 + tz.avg * 4 + tz2.avg * 2 + tejump.avg * 3 + tebomb.avg * 8 + tq.avg * 16
     }
   }
+}, {
+  title: '【魔导·秘仪】「嘭嘭轰击」伤害',
+  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2')
+}, {
+  title: '【魔导·秘仪】「嘭嘭轰击」蒸发伤害',
+  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2', 'vaporize')
+}, {
+  title: '4命Q后额外爆炸伤害',
+  params: { cons_4: true, cons_6: true },
+  cons: 4,
+  dmg: ({}, dmg) => dmg(555)
+}, {
+  title: '【魔导·秘仪】6命Q后「嘭嘭轰击」伤害',
+  params: { cons_6: true },
+  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2')
+}, {
+  title: '【魔导·秘仪】6命Q后「嘭嘭轰击」蒸发伤害',
+  params: { cons_6: true },
+  dmg: ({ talent }, dmg) => dmg(talent.a['重击伤害'], 'a2', 'vaporize')
 }
 ]
 
@@ -40,7 +59,8 @@ export const defDmgIdx = 5
 export const mainAttr = 'atk,cpct,cdmg,mastery'
 
 export const defParams = {
-  team: true
+  team: true,
+  Hexenzirkel: true // 魔女会成员
 }
 
 export const buffs = [{
@@ -113,4 +133,24 @@ export const buffs = [{
     kx: 40,
     mastery: 200
   }
+}, {
+  title: '可莉天赋：获得【魔导·秘仪】效果时，3枚「轰轰勋章」使特殊重击「嘭嘭轰击」造成原本150%的伤害。',
+  data: {
+    a2Multi: 50
+  }
+}, {
+  title: '可莉1命：攻击与施放技能时，有几率召唤火花。触发该效果后的12秒内，可莉的攻击力提升60%',
+  cons: 1,
+  data: {
+    atkPct: 60
+  }
+}, {
+  check: ({ params }) => params.cons_4 === true,
+  title: '可莉4命：触发额外爆炸时，如果可莉在场上，则造成的伤害提升100%',
+  cons: 4,
+  data: {
+    dmg: 100
+  }
 }, 'vaporize']
+
+export const createdBy = '喵喵 & 冰翼'
